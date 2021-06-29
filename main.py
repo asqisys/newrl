112ad92324d60782883121077de4f1905c4fefe6
Kousthub <kousthub.raja@gmail.com>
2021-06-29 14:22:42 +0530

    Scaffold more APIs

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ async def validate():
     except Exception as e:
         return {"status": "ERROR", "response": str(e)}    
 
+@app.post("/add-wallet")
+async def validate():
+    return {"status": "SUCCESS"}
+
+@app.post("/sign-transaction")
+async def validate():
+    return {"status": "SUCCESS"}
+
+@app.post("/create-token")
+async def validate():
+    return {"status": "SUCCESS"}
+
+@app.post("/run-updater")
+async def validate():
+    return {"status": "SUCCESS"}
+
 
 if __name__ == "__main__":
 	uvicorn.run(app, host="0.0.0.0", port=9500)
