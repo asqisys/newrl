61526f67f6fcc7c8c912bc82e19eac313e17f4c9
Kousthub Raja <kousthub.raja@gmail.com>
2021-06-30 23:21:12 +0530

    Add-wallet api

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from codes.addwallet import add_wallet
+from fastapi.datastructures import UploadFile
+from fastapi.params import File
 import uvicorn
 from fastapi import FastAPI
 
@@ -15,8 +18,21 @@ async def validate():
         return {"status": "ERROR", "response": str(e)}    
 
 @app.post("/add-wallet")
-async def validate():
-    return {"status": "SUCCESS"}
+async def validate(custodian_address: str = "0x7e433fd1cc776d17d4ad94daa2e1fc52ef967b42",
+     kyc1: UploadFile = File(...),
+                    kyc2: UploadFile = File(...)):
+    
+    print(kyc1.file, kyc2.file.name)
+    file_location = f"/tmp/{kyc1.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(kyc1.file.read())
+    f1 = file_location
+    f2 = file_location
+    newaddress = add_wallet(f1, f2, custodian_address)
+    return {
+        "status": "SUCCESS",
+        "new_address": newaddress
+    }
 
 @app.post("/sign-transaction")
 async def validate():
