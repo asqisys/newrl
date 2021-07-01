86675eead81aa300f78dabb4a63b39357a18ce9b
Kousthub Raja <kousthub.raja@gmail.com>
2021-07-01 07:05:58 +0530

    Add more APIs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from utils import save_file_and_get_path
+from codes.transactionmanager import Transactionmanager
 from codes.addwallet import add_wallet
 from fastapi.datastructures import UploadFile
 from fastapi.params import File
@@ -5,45 +7,44 @@ import uvicorn
 from fastapi import FastAPI
 
 from codes import validator
+from codes import signmanager
+from codes import validator
 
 app = FastAPI()
 
 
 @app.post("/validate")
-async def validate():
-    try:
-        response = validator.main()
-        return {"status": "SUCCESS", "response": response}
-    except Exception as e:
-        return {"status": "ERROR", "response": str(e)}    
-
+async def validate(transactionfile: UploadFile = File(...)):
+    transactionfile_path = save_file_and_get_path(transactionfile)
+    response = validator.validate(transactionfile_path)
+    return {"status": "SUCCESS", "response": response}
+    
 @app.post("/add-wallet")
 async def validate(custodian_address: str = "0x7e433fd1cc776d17d4ad94daa2e1fc52ef967b42",
-     kyc1: UploadFile = File(...),
-                    kyc2: UploadFile = File(...)):
-    
-    print(kyc1.file, kyc2.file.name)
-    file_location = f"/tmp/{kyc1.filename}"
-    with open(file_location, "wb+") as file_object:
-        file_object.write(kyc1.file.read())
-    f1 = file_location
-    f2 = file_location
+    kyc1: UploadFile = File(...), kyc2: UploadFile = File(...)):
+    f1 = save_file_and_get_path(kyc1)
+    f2 = save_file_and_get_path(kyc2)
     newaddress = add_wallet(f1, f2, custodian_address)
     return {
         "status": "SUCCESS",
         "new_address": newaddress
     }
 
-@app.post("/sign-transaction")
-async def validate():
-    return {"status": "SUCCESS"}
+@app.post("/sign")
+async def sign(custodian_address: str = "0x7e433fd1cc776d17d4ad94daa2e1fc52ef967b42", transactionfile: UploadFile = File(...)):
+    transactionfile_path = save_file_and_get_path(transactionfile)
+    sign_status = signmanager.sign(custodian_address, transactionfile_path)
+    return {
+        "status": "SUCCESS",
+        "data": sign_status
+    }
 
 @app.post("/create-token")
-async def validate():
+async def create_token():
     return {"status": "SUCCESS"}
 
 @app.post("/run-updater")
-async def validate():
+async def run_updater():
     return {"status": "SUCCESS"}
 
 
