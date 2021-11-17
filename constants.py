3540e2f9062be53877295369b40ddbac663c50ed
Kousthub Raja <kousthub.raja@gmail.com>
2021-11-18 02:00:54 +0530

    add constants file

---
+++ constants.py
@@ -0,0 +1,9 @@
+# Global constants in this file
+
+MEMPOOL_PATH = 'data/mempool'
+STATE_DB = 'newrl_state.db'
+CHAIN_DB = 'newrl_chain.db'
+PORT = 8080
+STATE_FILE = 'state.json'
+CHAIN_FILE = 'chain.json'
+
