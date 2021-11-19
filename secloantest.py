553c73e90f82819b8e289f00592cfeb3460eaf7f
Swapnil Pawar <spawar@asqi.in>
2021-11-19 16:13:06 +0530

    adding secloan contract

---
+++ secloantest.py
@@ -0,0 +1,37 @@
+import json
+from codes import secloanv100
+def test_secloan1():
+    callparams = {"contractparams": 
+    {
+        "creator":"addressofcreator",
+        "name":"secloan1",
+        "version":"1.0.0",
+        "actmode":"hybrid",
+        "status":0,
+        "next_act_ts":None,
+        "signatories":[],
+        "parentcontractaddress":None,
+        "oracleids":[],
+        "selfdestruct":None,
+        "contractspecs":{
+                "tokencode":0,
+                "loanamount":0,
+                "repayment":0,
+                "due_date":None,
+                "ltv":0,
+                "sec_token_code":0,
+                "sec_token_amount":0,
+                "borrowerwallet":None,
+                "lenderwallet":None,
+                "secprovider":None,
+                "special_params":{}
+                },
+            "legalparams":{}
+        }
+    }
+    callparamjson=json.dumps(callparams)
+    loan1=secloanv100.SecLoan1()
+    loan1.setup(callparamjson)
+    loan1.create_loan_token()
+if __name__ == '__main__':
+    test_secloan1()
