77443627e4fbe392b64b11c93cff761d7301d0c9
Kousthub Raja <kousthub.raja@gmail.com>
2021-12-09 10:30:05 +0530

    sync node from node testing

--- p2p_main.py
+++ p2p_main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from codes.p2p.sync_chain import get_block, get_blocks, get_last_block_index
+from codes.p2p.sync_chain import get_block, get_blocks, get_last_block_index, sync_chain_from_node
 
 from codes.p2p.sync_mempool import get_mempool_transactions, list_mempool_transactions, sync_mempool_transactions
 from p2p_request_models import TransactionsRequest
@@ -24,13 +24,17 @@ async def get_mempool_transactions_api(req: TransactionsRequest):
 async def get_mempool_transactions_api(req: TransactionsRequest):
     return get_blocks(req.transaction_codes)
 
-@app.post("/get-last-block-index")
+@app.get("/get-last-block-index")
 async def get_last_block_index_api():
     return get_last_block_index()
 
 @app.post("/sync-mempool-transactions")
 async def sync_mempool_transactions_api():
     return sync_mempool_transactions()
+
+@app.post("/sync-chain-from-node")
+async def sync_chain_from_node_api(url: str = 'https://newrl-devnet1.herokuapp.com'):
+    return sync_chain_from_node(url)
 
 
 if __name__ == "__main__":
