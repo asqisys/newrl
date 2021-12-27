474b7c3ea2dcb8a6d9b283062e7d9327fc318ff3
Kousthub Raja <kousthub.raja@gmail.com>
2021-12-27 07:38:16 +0530

    fix start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,9 @@ git checkout p2p
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python app/migrations/init_newrl.py
+python -m app.migrations.init
 # cd migrations
 # python migrate_chain.py
 # python migrate_state.py
 # cd ..
-python app/p2p_main.py & app/python main.py
+python -m app.main
