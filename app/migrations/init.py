3bb6c9669f5c9abb464bff9b73d2d707189a2d94
Kousthub Raja <kousthub.raja@gmail.com>
2021-12-30 11:09:26 +0530

    fix init script

--- app/migrations/init.py
+++ app/migrations/init.py
@@ -1,9 +1,11 @@
 import os
-from ..constants import INCOMING_PATH, MEMPOOL_PATH, TMP_PATH
+from ..constants import INCOMING_PATH, MEMPOOL_PATH, TMP_PATH, DATA_PATH
 from ..migrations.init_db import clear_db, init_db
 
 
 if __name__ == '__main__':
+    if not os.path.exists(DATA_PATH):
+        os.mkdir(DATA_PATH)
     if not os.path.exists(MEMPOOL_PATH):
         os.mkdir(MEMPOOL_PATH)
     if not os.path.exists(TMP_PATH):
