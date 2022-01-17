ff417569f60faccff231b8882abd35dc9b230b90
Swapnil Pawar <spawar@asqi.in>
2022-01-17 23:05:07 +0530

    stable transfermanager

--- app/codes/transfermanager.py
+++ app/codes/transfermanager.py
@@ -10,16 +10,8 @@ from .transactionmanager import Transactionmanager
 class Transfermanager(Transactionmanager):
     def __init__(self, transfer_data):
         Transactionmanager.__init__(self)
-        self.transaction = {
-                "timestamp": str(datetime.datetime.now()),
-                "trans_code": uuid.uuid4().hex,
-                "type": int(transfer_data['transfer_type']),
-                "currency": "INR",
-                "fee": 0.0,
-                "descr": "",
-                "valid": 1,
-                "specific_data": transfer_data
-            }
+        self.transaction = transfer_data['transaction']
+        self.fulltrandata = transfer_data
 
     def techvalid(self):
         return True
@@ -28,7 +20,7 @@ class Transfermanager(Transactionmanager):
     def loadandcreate(self, transfer_data=None):
     #    self.transferdata = transfer_data
     #    tdataall = {"transaction":self.transferdata, "signatures":[]} 
-        tdatawithcode = self.transactioncreator(self.transferdata)
-        if self.techvalid():
-            self.dumptransaction()
+        tdatawithcode = self.transactioncreator(self.fulltrandata)
+    #    if self.techvalid():
+    #        self.dumptransaction()
         return tdatawithcode
