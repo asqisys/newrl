9fd6813795fa628e0db6c7cd5476ed52772df923
Kousthub Raja <kousthub.raja@gmail.com>
2022-02-27 08:35:36 +0530

    fix syntax errors after flake8

--- app/codes/consensus/committee.py
+++ app/codes/consensus/committee.py
@@ -7,7 +7,7 @@ from ..p2p.peers import get_peers
 
 def get_current_committee():
     """Return the current committee members""" 
-    new_block_idx = last_block_idx + 1
+    # new_block_idx = last_block_idx + 1
     peers = get_peers()
     committee = peers[:5]  # TODO - Find the committe based on last block hash
     # use select_from function to select from peers
