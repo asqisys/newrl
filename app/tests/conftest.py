87d2f2259c2203a749bef21370015ec92299e7e9
Kousthub Raja <kousthub.raja@gmail.com>
2022-02-27 17:14:50 +0530

    fix test fixture

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import pytest
 
+from ..codes.p2p.peers import init_peer_db
 
 def setup_test_files():
     """Setup test files"""
@@ -10,7 +11,10 @@ def setup_test_files():
         os.makedirs('data_test/')
     if os.path.exists('data_test/newrl.db'):
         os.remove('data_test/newrl.db')
+    if os.path.exists('data_test/newrl_p2p.db'):
+        os.remove('data_test/newrl_p2p.db')
     shutil.copyfile('data_test/template/newrl.db', 'data_test/newrl.db')
+    init_peer_db()
 
 
 @pytest.fixture(scope="session", autouse=True)
