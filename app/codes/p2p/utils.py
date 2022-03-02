9b086fed2ab3dc6e37a6e625de2d7db394ab60b1
Kousthub Raja <kousthub.raja@gmail.com>
2022-03-02 10:34:51 +0530

    fix recursive propogation transaction and blocks

--- app/codes/p2p/utils.py
+++ app/codes/p2p/utils.py
@@ -1,5 +1,6 @@
 import sqlite3
-
+import requests
+import socket
 from ...constants import NEWRL_P2P_DB
 
 
@@ -11,3 +12,14 @@ def get_peers():
     peer_cursor = cur.execute('SELECT * FROM peers').fetchall()
     peers = [dict(ix) for ix in peer_cursor]
     return peers
+
+
+def get_my_address():
+    return requests.get('https://api.ipify.org?format=json').json()['ip']
+
+
+def is_my_address(address):
+    my_address = get_my_address()
+    if socket.gethostbyname(address) == my_address:
+        return True
+    return False
