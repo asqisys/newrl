3fe9e8af5e9c6c092ea67a915819e5ab52f53d1c
Kousthub Raja <kousthub.raja@gmail.com>
2022-03-30 13:33:55 +0530

    fix miner cutoff time

--- app/codes/minermanager.py
+++ app/codes/minermanager.py
@@ -84,7 +84,7 @@ def get_eligible_miners():
     # else:
     #     cutfoff_epoch = 0
     last_block_epoch = int(last_block['timestamp'])
-    cutfoff_epoch = last_block_epoch + TIME_MINER_BROADCAST_INTERVAL_SECONDS * 1000
+    cutfoff_epoch = last_block_epoch - TIME_MINER_BROADCAST_INTERVAL_SECONDS * 1000
 
     con = sqlite3.connect(NEWRL_DB)
     con.row_factory = sqlite3.Row
