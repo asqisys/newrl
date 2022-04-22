82f481325864099afcfa83be11406f7962617013
prannav-k <prannavk@gmail.com>
2022-04-22 15:05:43 +0530

    log creation and stream

--- app/routers/system.py
+++ app/routers/system.py
@@ -5,6 +5,8 @@ import uvicorn
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from starlette.requests import Request
+from app.codes.log_config import logGenerator
+from sse_starlette.sse import EventSourceResponse
 
 from app.codes.chainscanner import download_chain, download_state
 from app.codes.clock.global_time import get_time_stats
@@ -121,3 +123,8 @@ def update_software_api(propogate: bool = False):
     timer = threading.Timer(randint(30, 60), update_software, [propogate])
     timer.start()
     return {'status': 'SUCCESS'}
+
+@router.get('/stream-logs',tags=[p2p_tag])
+async def run(request: Request):
+    event_generator = logGenerator(request)
+    return EventSourceResponse(event_generator)
