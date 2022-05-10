f03e5acba386a67f1140764fd6428fb4eada91ee
prannav-k <prannavk@gmail.com>
2022-05-10 17:37:28 +0530

    Refactored token dao calss names

--- app/codes/contracts/token_dao.py
+++ app/codes/contracts/token_dao.py
@@ -1,11 +1,11 @@
 from .dao_main_template import DaoMainTemplate
 from ..db_updater import *
 
-class asqi_fund_dao(DaoMainTemplate):
+class token_dao(DaoMainTemplate):
     codehash=""
 
     def __init__(self, contractaddress=None):
-        self.template= 'asqi_fund_dao'
+        self.template= 'token_dao'
         self.version='1.0.0'
         self.dao_type=2
         # dao_type=2 is for token based DAO and 1 is for membership DAO
